c4dceb39ee9660c82f5af1991138f0f69db9cf73
ivan <ivan.cmartin94@gmail.com>
2020-08-24 10:54:36 +0200

    aprendiendo python 4

---
+++ gui/ejemplo_interfaces.py
@@ -0,0 +1,9 @@
+from tkinter import *
+raiz=Tk()
+raiz.title("Ventana de prueba")
+raiz.resizable(1,1)
+raiz.geometry("650x350")
+raiz.config(bg='grey')
+#raiz.iconbitmap("icono.ico")
+
+raiz.mainloop()#Esta instruccion simepre se coloca al final
